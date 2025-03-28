e3726a46ed29cd1d46fed12bc0dd729b0794d525
warramma <wrammazy@mail.bradley.edu>
2025-03-28 16:13:00 -0500

    update background color and active page color in tab

--- Neighborhood-Safety-App/app/(tabs)/_layout.tsx
+++ Neighborhood-Safety-App/app/(tabs)/_layout.tsx
@@ -1,8 +1,14 @@
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false, // Hides the title at the top
+        tabBarActiveTintColor: '#14B8A6',
+      }}
+      >
       <Tabs.Screen name="index" options={{ title: 'Home' }} />
       <Tabs.Screen name="map" options={{title: 'Map'}}/>
       <Tabs.Screen name="reports" options={{title: 'Reports'}}/>
--- Neighborhood-Safety-App/app/(tabs)/reports.tsx
+++ Neighborhood-Safety-App/app/(tabs)/reports.tsx
@@ -11,7 +11,7 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#F8FAFC',
     justifyContent: 'center',
     alignItems: 'center',
   },
