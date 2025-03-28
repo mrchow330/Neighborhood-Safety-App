55e2e9a4ebe87ee2b5d63f9b50c28f8e18b81e0a
warramma <wrammazy@mail.bradley.edu>
2025-03-28 16:38:00 -0500

    add icons to tab

--- Neighborhood-Safety-App/app/(tabs)/_layout.tsx
+++ Neighborhood-Safety-App/app/(tabs)/_layout.tsx
@@ -1,18 +1,28 @@
 import { Tabs } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false, // Hides the title at the top
-        tabBarActiveTintColor: '#14B8A6',
+        tabBarActiveTintColor: '#76B6FF',
       }}
       >
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="map" options={{title: 'Map'}}/>
-      <Tabs.Screen name="reports" options={{title: 'Reports'}}/>
-      <Tabs.Screen name="account" options={{title: 'Account'}}/>
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+       }} />
+      <Tabs.Screen name="map" options={{title: 'Map', tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "map-sharp" : "map-outline"} size={24} color={color} />
+          ),}}/>
+      <Tabs.Screen name="reports" options={{title: 'Reports', tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "alert-circle-sharp" : "alert-circle-outline"} size={24} color={color} />
+          ),}}/>
+      <Tabs.Screen name="account" options={{title: 'Account', tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons name={focused ? "account-circle" : "account-circle-outline"} size={24} color={color} />
+          ),}}/>
     </Tabs>
   );
 }
--- Neighborhood-Safety-App/app/(tabs)/map.tsx
+++ Neighborhood-Safety-App/app/(tabs)/map.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function AboutScreen() {
+export default function MapScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Map</Text>
