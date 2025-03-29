19600bc9faddb67467592ce1ef24eceab32320d2
warramma <wrammazy@mail.bradley.edu>
2025-03-28 19:08:30 -0500

    add stack and rootStackParamList

--- Neighborhood-Safety-App/app/_layout.tsx
+++ Neighborhood-Safety-App/app/_layout.tsx
@@ -1,10 +1,28 @@
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false, // Hides the title at the top
+    }}
+    >
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
+      <Stack.Screen name="about" options={{title:'About'}}/>
+      <Stack.Screen name="reportIssue" options={{title: 'Report Issue', tabBarStyle: { display: 'flex' }}}/>
     </Stack>
   );
 }
+
+export type RootStackParamList = {
+  "(tabs)": undefined;  // This screen has no parameters
+  "+not-found": undefined;  
+  about: undefined;  
+  reportIssue: undefined;  
+  index: undefined;  
+  map: undefined;  
+  reports: undefined;  
+  account: undefined;  
+};
