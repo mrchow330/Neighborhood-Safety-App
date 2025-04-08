8b1b216b0e9c71aa6134486cd65f1d2c600c0824
“mrchow330 <echow@mail.bradley.edu>
2025-04-08 15:03:40 -0500

    Downloaded dependenies

--- Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
+++ Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
@@ -1,22 +1,21 @@
-import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
-export default function MapScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map</Text>
-    </View>
-  );
-}
+const API_KEY =
+  globalThis.GOOGLE_MAPS_API_KEY ?? ("YOUR_API_KEY");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F8FAFC',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#1E293B',
-    fontFamily: 'Nunito_400Regular'
-  },
-});
+const App = () => (
+  <APIProvider
+    solutionChannel='GMP_devsite_samples_v3_rgmbasicmap'
+    apiKey={API_KEY}>
+    <Map
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
+);
+const root = createRoot(document.getElementById('app')!);
+root.render(<App />);
