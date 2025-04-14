35bfd93d556034055ef0a0e55b6967df36095487
warramma <wrammazy@mail.bradley.edu>
2025-04-14 07:15:48 -0500

    add logout to account.tsx

--- Neighborhood-Safety-App/app/(app)/(drawer)/account.tsx
+++ Neighborhood-Safety-App/app/(app)/(drawer)/account.tsx
@@ -1,9 +1,17 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+import {useSession} from '../../../components/ctx';
 
 export default function AccountScreen() {
+  const {signOut} = useSession();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Accounts</Text>
+
+      {/*add profile, username, etc here*/}
+
+      <Pressable style={styles.logoutButton} onPress = {signOut}>
+        <Text style={styles.logoutText}>Log Out</Text>
+      </Pressable>
     </View>
   );
 }
@@ -18,5 +26,17 @@ const styles = StyleSheet.create({
   text: {
     color: '#1E293B',
     fontFamily: 'Nunito_400Regular'
+  },
+  logoutButton: {
+    marginTop: 24,
+    backgroundColor: '#EF4444',
+    paddingVertical: 10,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+  },
+  logoutText: {
+    color: 'white',
+    fontFamily: 'Nunito_600SemiBold',
+    fontSize: 16,
   },
 });
