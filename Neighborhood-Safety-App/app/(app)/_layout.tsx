ea896af56bef19ffa9698fc9bc1ae21a45dce506
warramma <wrammazy@mail.bradley.edu>
2025-04-14 07:01:40 -0500

    delete unused files, restrict access to app

--- Neighborhood-Safety-App/app/(app)/_layout.tsx
+++ Neighborhood-Safety-App/app/(app)/_layout.tsx
@@ -1,47 +1,36 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import {Text} from "react-native";
+import {useSession} from '../../components/ctx'; 
 
-export default function RootLayout() {
+export default function AppLayout(){
+  const {session, isLoading} = useSession();
+
+  if (isLoading){
+    //splash screen
+    return <Text>Loading...</Text>
+  }
+
+  if (!session){
+    //redirect to sign in if not signed in
+    return <Redirect href="/sign-in"/>;
+  }
   return (
-    <Stack>
-      {/* Main drawer layout is the default */}
-      <Stack.Screen 
-        name="(drawer)" 
-        options={{ headerShown: false, title:"" }} 
-      />
+        <Stack>
+          {/* Main drawer layout is the default */}
+          <Stack.Screen 
+            name="(drawer)" 
+            options={{ headerShown: false, title:"" }} 
+          />
+    
+          {/* Report Issue is pushed on top of the drawer */}
+          <Stack.Screen 
+            name="reportIssue" 
+            options={{ title: "Report Issue" }} 
+          />
+        </Stack>
+      );
 
-      {/* Report Issue is pushed on top of the drawer */}
-      <Stack.Screen 
-        name="reportIssue" 
-        options={{ title: "Report Issue" }} 
-      />
-    </Stack>
-  );
 }
-
-
-// import { Drawer } from 'expo-router/drawer';
-// import { Stack } from 'expo-router'; 
-// import TabLayout from './(drawer)/(tabs)/_layout'; 
-
-// export default function Layout() {
-//   return (
-//     <Stack>
-//       {/* Stack screen for Report Issue */}
-//       <Stack.Screen
-//         name="reportIssue"
-//         options={{ title: 'Report Issue' }}
-//       />
-
-//       {/* Default drawer screen */}
-//       <Drawer>
-//         <Drawer.Screen name="tabs" options={{ headerShown: true, title:'' }} />
-//         <Drawer.Screen name="about" options={{ title: "About" }} />
-//         <Drawer.Screen name="account" options={{ title: "Account" }} />
-//       </Drawer>
-//     </Stack>
-//   );
-// }
-
 
 export type RootStackParamList = {
   "(tabs)": undefined;  // This screen has no parameters
@@ -54,3 +43,22 @@ export type RootStackParamList = {
   account: undefined;  
   communityGuidlines : undefined;
 };
+
+
+// export default function RootLayout() {
+//   return (
+//     <Stack>
+//       {/* Main drawer layout is the default */}
+//       <Stack.Screen 
+//         name="(drawer)" 
+//         options={{ headerShown: false, title:"" }} 
+//       />
+
+//       {/* Report Issue is pushed on top of the drawer */}
+//       <Stack.Screen 
+//         name="reportIssue" 
+//         options={{ title: "Report Issue" }} 
+//       />
+//     </Stack>
+//   );
+// }
