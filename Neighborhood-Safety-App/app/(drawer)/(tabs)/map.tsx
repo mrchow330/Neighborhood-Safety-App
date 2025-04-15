907e230acd4c850125ec79151b6aa6e72a721ad4
“mrchow330 <echow@mail.bradley.edu>
2025-04-15 18:20:07 -0500

    Tried removing map.fitbounds in const onLoad

--- Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
+++ Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
@@ -30,10 +30,6 @@ function MyComponent() {
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-
     setMap(map)
   }, [])
 
