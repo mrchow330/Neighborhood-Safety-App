444350b5d903c4d4f1657b6edb047c2c74ba968d
Masonbach03 <mbreidenbach@mail.bradley.edu>
2025-04-15 17:26:34 -0500

    update map
    width

--- Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
+++ Neighborhood-Safety-App/app/(drawer)/(tabs)/map.tsx
@@ -3,7 +3,7 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 import { GOOGLE_MAPS_API_KEY } from '../../../config';
 
 const containerStyle = {
-  width: '1500px',
+  width: '1550px',
   height: '1000px',
 }
 
